a985bbed86e6d3347c43ef29ced9b6e7c1fdbb58
Nicky Meuleman <nicky.dev@outlook.com>
2024-04-14 14:20:20 +0200

    nvim: indentation guide with inden blankline
    Uses color highlight colours from rainbow-delimiters plugin

--- .config/nvim/lua/nicky/plugins/indent-blankline.lua
+++ .config/nvim/lua/nicky/plugins/indent-blankline.lua
@@ -1,10 +1,32 @@
 return {
   "lukas-reineke/indent-blankline.nvim",
+  dependencies = {
+    "HiPhish/rainbow-delimiters.nvim"
+  },
   main = "ibl",
   config = function()
+    -- use same highlight groups as the rainbow-delimiter plugins
+    -- this way the brackets that delimit a scope in many languages has the same colour as its indentline
+    local highlight = {
+      "RainbowDelimiterRed",
+      "RainbowDelimiterYellow",
+      "RainbowDelimiterBlue",
+      "RainbowDelimiterOrange",
+      "RainbowDelimiterGreen",
+      "RainbowDelimiterViolet",
+      "RainbowDelimiterCyan",
+    }
+
+    local hooks = require("ibl.hooks")
+
+    vim.g.rainbow_delimiters = { highlight = highlight }
+
     require("ibl").setup({
       enabled = true,
       indent = { char = "▏" },
+      scope = { highlight = highlight }
     })
+
+    hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
   end
 }
