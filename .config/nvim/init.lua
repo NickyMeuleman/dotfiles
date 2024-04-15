7e3a1b6e1b919342a85c36f6aa9f268c32dadb22
Nicky Meuleman <nicky.dev@outlook.com>
2024-04-15 21:11:32 +0200

    nvim: highlight yanked text

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -3,3 +3,12 @@ require("nicky.options")
 vim.g.mapleader = " " -- character that replaces the special <leader> mapping in keybinds
 require("nicky.keymaps")
 require("nicky.lazy")
+
+-- Highlight on yank
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "Highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank({ timeout = 250 })
+	end,
+})
