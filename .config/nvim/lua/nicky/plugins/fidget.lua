90b0562e02bc5feffb5d02d323ac2dc7eaee5a3c
Nicky Meuleman <nicky.dev@outlook.com>
2024-04-18 16:59:32 +0200

    nvim: adjust fidget so it does not get stuck on rust-analyzer

--- .config/nvim/lua/nicky/plugins/fidget.lua
+++ .config/nvim/lua/nicky/plugins/fidget.lua
@@ -1,6 +1,17 @@
 return {
 	"j-hui/fidget.nvim",
 	config = function()
-		require("fidget").setup({})
+		require("fidget").setup({
+			progress = {
+				lsp = {
+					-- https://github.com/j-hui/fidget.nvim/issues/201
+					progress_ringbuf_size = 2048,
+				},
+				display = {
+					-- hide messages that have been shown for a long time
+					progress_ttl = 180,
+				},
+			},
+		})
 	end,
 }
