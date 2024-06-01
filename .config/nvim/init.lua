3ef79577de5bba449b9a9a59bc42eb0885050c97
Nicky Meuleman <nicky.dev@outlook.com>
2024-06-01 17:44:41 +0200

    nvim: lualine setup

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -26,10 +26,10 @@ vim.filetype.add({
 	},
 })
 
-vim.fn.sign_define("DiagnosticSignError", { text = " ", texthl = "DiagnosticSignError" })
-vim.fn.sign_define("DiagnosticSignWarn", { text = " ", texthl = "DiagnosticSignWarn" })
-vim.fn.sign_define("DiagnosticSignInfo", { text = " ", texthl = "DiagnosticSignInfo" })
-vim.fn.sign_define("DiagnosticSignHint", { text = "󰌵", texthl = "DiagnosticSignHint" })
+vim.fn.sign_define("DiagnosticSignError", { text = "󰅚 ", texthl = "DiagnosticSignError" })
+vim.fn.sign_define("DiagnosticSignWarn", { text = "󰀪 ", texthl = "DiagnosticSignWarn" })
+vim.fn.sign_define("DiagnosticSignInfo", { text = "󰋽 ", texthl = "DiagnosticSignInfo" })
+vim.fn.sign_define("DiagnosticSignHint", { text = "󰌶 ", texthl = "DiagnosticSignHint" })
 
 require("nicky.lazy")
 
--- .config/nvim/lua/nicky/plugins/lualine.lua
+++ .config/nvim/lua/nicky/plugins/lualine.lua
@@ -8,6 +8,13 @@ return {
 		require("lualine").setup({
 			options = {
 				theme = "catppuccin",
+				component_separators = { left = "", right = "" },
+				section_separators = { left = "", right = "" },
+			},
+			sections = {
+				lualine_x = {
+					{ "filetype" },
+				},
 			},
 		})
 	end,
