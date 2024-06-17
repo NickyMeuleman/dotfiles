d4d53ee283bbb5562be557f443c3600c89688bca
Nicky Meuleman <nicky.dev@outlook.com>
2024-06-17 20:38:04 +0200

    nvim: setup todo-comments

--- .config/nvim/lua/nicky/plugins/todo-comments.lua
+++ .config/nvim/lua/nicky/plugins/todo-comments.lua
@@ -1,9 +1,60 @@
 return {
 	"folke/todo-comments.nvim",
-	dependencies = { "nvim-lua/plenary.nvim" },
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+	},
 	cmd = { "TodoTrouble", "TodoTelescope" },
 	event = { "BufReadPost", "BufNewFile" },
+	keys = {
+		{
+			"]t",
+			function()
+				require("todo-comments").jump_next()
+			end,
+			desc = "Next todo comment",
+		},
+		{
+			"[t",
+			function()
+				require("todo-comments").jump_prev()
+			end,
+			desc = "Previous todo comment",
+		},
+	},
 	config = function()
-		require("todo-comments").setup({})
+		require("todo-comments").setup({
+			keywords = {
+				FIX = { icon = " ", color = "error", alt = { "FIXME", "BUG", "FIXIT", "ISSUE" } },
+				HACK = { icon = " ", color = "warning" },
+				NOTE = { icon = " ", color = "hint", alt = { "INFO" } },
+				PERF = { icon = " ", color = "perf", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
+				TEST = { icon = " ", color = "test", alt = { "TESTING", "PASSED", "FAILED" } },
+				TODO = { icon = " ", color = "info" },
+				WARN = { icon = " ", color = "warning", alt = { "WARNING", "XXX" } },
+			},
+			colors = {
+				default = { "Identifier" },
+				error = { "DiagnosticError" },
+				hint = { "DiagnosticHint"},
+				info = { "DiagnosticInfo" },
+				perf = { "@conditional"},
+				test = { "@function" },
+				warning = { "DiagnosticWarn"},
+			}
+		})
 	end,
 }
+-- FIX:
+-- multiline test
+-- WARN:
+-- multiline test
+-- TEST:
+-- multiline test
+-- HACK:
+-- multiline test
+-- TODO:
+-- multiline test
+-- NOTE:
+-- multiline test
+-- PERF:
+-- multiline test
