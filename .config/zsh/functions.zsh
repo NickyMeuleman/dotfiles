d9e81ea701a2270b1d5f3a70ff295abd6df24436
Nicky Meuleman <nicky.dev@outlook.com>
2025-02-09 16:49:17 +0100

    zsh: formatting

--- .config/zsh/functions.zsh
+++ .config/zsh/functions.zsh
@@ -1,47 +1,47 @@
 function zsh_add_file() {
-    [ -f "$ZDIR/$1" ] && source "$ZDIR/$1"
+	[ -f "$ZDIR/$1" ] && source "$ZDIR/$1"
 }
 
 function zsh_add_plugin() {
-    PLUGIN_NAME=$(echo $1 | cut -d "/" -f 2)
-    if [ -d "$ZDIR/plugins/$PLUGIN_NAME" ]; then 
-        zsh_add_file "plugins/$PLUGIN_NAME/$PLUGIN_NAME.plugin.zsh" || \
-        zsh_add_file "plugins/$PLUGIN_NAME/$PLUGIN_NAME.zsh"
-    else
-        git clone "https://github.com/$1.git" "$ZDIR/plugins/$PLUGIN_NAME"
-    fi
+	PLUGIN_NAME=$(echo $1 | cut -d "/" -f 2)
+	if [ -d "$ZDIR/plugins/$PLUGIN_NAME" ]; then
+		zsh_add_file "plugins/$PLUGIN_NAME/$PLUGIN_NAME.plugin.zsh" ||
+			zsh_add_file "plugins/$PLUGIN_NAME/$PLUGIN_NAME.zsh"
+	else
+		git clone "https://github.com/$1.git" "$ZDIR/plugins/$PLUGIN_NAME"
+	fi
 }
 
 function install_completions() {
-    # prompt: starship
-    starship completions zsh > $ZDIR/completions/_starship
-    # GitHub CLI: gh
-    gh completion -s zsh > $ZDIR/completions/_gh
-    # Rust language: rustup
-    rustup completions zsh > $ZDIR/completions/_rustup
-    # Rust language package tool: cargo
-    rustup completions zsh cargo > $ZDIR/completions/_cargo
-    # node manager: fnm
-    fnm completions --shell=zsh > $ZDIR/completions/_fnm
-    # better cat: bat
-    echo 'manually copy bat completions from the /out/assets/completions folder inside target/release'
-    # build bat with `cargo build --release` and copy the completions from the built output
-    # cp $HOME/projects/sharkdp/bat/target/release/build/bat-c95ebc37c4f6628f/out/assets/completions/bat.zsh $ZDIR/completions/_bat
-    # smart cd: zoxide
-    echo 'manually copy zoxide completions from the /contrib/completions folder'
-    # cp $HOME/projects/ajeetdsouza/zoxide/contrib/completions/_zoxide $ZDIR/completions/_zoxide
-    # better find: fd
-    # This command works, but the repo has a better completions file in it you should use instead
-    # fd --gen-completions zsh > $ZDIR/completions/_fd
-    # see: https://github.com/sharkdp/fd/commit/ef1bfc750862b751de1e235a5bf7e112c5378187
-    echo 'manually copy fd completions from the /contrib/completion folder'
-    # cp $HOME/projects/sharkdp/fd/contrib/completion/_fd $ZDIR/completions/_fd
-    # better `make`: `just`
-    just --completions zsh > $ZDIR/completions/_just
-    # CLI benchmarks: `hyperfine`
-    # The maintainer doesn't like this approach of generating completions: https://github.com/sharkdp/hyperfine/issues/293
-    # cp $HOME/projects/sharkdp/hyperfine/target/release/build/hyperfine-813f970f0b8c8f10/out/_hyperfine $ZDIR/completions/_hyperfine
-    echo 'manually copy hyperfine completions from the /out folder inside target/release/build/hyperfine-<some string>/'
+	# prompt: starship
+	starship completions zsh >$ZDIR/completions/_starship
+	# GitHub CLI: gh
+	gh completion -s zsh >$ZDIR/completions/_gh
+	# Rust language: rustup
+	rustup completions zsh >$ZDIR/completions/_rustup
+	# Rust language package tool: cargo
+	rustup completions zsh cargo >$ZDIR/completions/_cargo
+	# node manager: fnm
+	fnm completions --shell=zsh >$ZDIR/completions/_fnm
+	# better cat: bat
+	echo 'manually copy bat completions from the /out/assets/completions folder inside target/release'
+	# build bat with `cargo build --release` and copy the completions from the built output
+	# cp $HOME/projects/sharkdp/bat/target/release/build/bat-c95ebc37c4f6628f/out/assets/completions/bat.zsh $ZDIR/completions/_bat
+	# smart cd: zoxide
+	echo 'manually copy zoxide completions from the /contrib/completions folder'
+	# cp $HOME/projects/ajeetdsouza/zoxide/contrib/completions/_zoxide $ZDIR/completions/_zoxide
+	# better find: fd
+	# This command works, but the repo has a better completions file in it you should use instead
+	# fd --gen-completions zsh > $ZDIR/completions/_fd
+	# see: https://github.com/sharkdp/fd/commit/ef1bfc750862b751de1e235a5bf7e112c5378187
+	echo 'manually copy fd completions from the /contrib/completion folder'
+	# cp $HOME/projects/sharkdp/fd/contrib/completion/_fd $ZDIR/completions/_fd
+	# better `make`: `just`
+	just --completions zsh >$ZDIR/completions/_just
+	# CLI benchmarks: `hyperfine`
+	# The maintainer doesn't like this approach of generating completions: https://github.com/sharkdp/hyperfine/issues/293
+	# cp $HOME/projects/sharkdp/hyperfine/target/release/build/hyperfine-813f970f0b8c8f10/out/_hyperfine $ZDIR/completions/_hyperfine
+	echo 'manually copy hyperfine completions from the /out folder inside target/release/build/hyperfine-<some string>/'
 }
 
 # overwrite fzf functions that get called by trigger-sequence tabbing
@@ -52,9 +52,9 @@ function install_completions() {
 # - See the source code (completion.{bash,zsh}) for the details.
 # $@ means all argument separated by a space
 function _fzf_compgen_path() {
-    fd --hidden --follow --exclude ".git" . "$@"
+	fd --hidden --follow --exclude ".git" . "$@"
 }
 # `cd **<tab>` runs _fzf_compgen_dir()
 function _fzf_compgen_dir() {
-    fd --type directory --hidden --follow --exclude ".git" . "$@"
+	fd --type directory --hidden --follow --exclude ".git" . "$@"
 }
