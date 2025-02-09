e6c2f2a3d5ed7d4233d0b14beb8a65eb429550d8
Nicky Meuleman <nicky.dev@outlook.com>
2025-02-09 16:39:58 +0100

    nvim: move trouble from dev to prod branch

--- .config/nvim/lua/nicky/plugins/trouble.lua
+++ .config/nvim/lua/nicky/plugins/trouble.lua
@@ -1,6 +1,6 @@
 return {
 	"folke/trouble.nvim",
-	branch = "dev", -- IMPORTANT!
+	cmd = "Trouble",
 	keys = {
 		{
 			"<leader>xx",
@@ -33,5 +33,5 @@ return {
 			desc = "Quickfix List (Trouble)",
 		},
 	},
-	opts = {}, -- for default options, refer to the configuration section for custom setup.
+	opts = {},
 }
