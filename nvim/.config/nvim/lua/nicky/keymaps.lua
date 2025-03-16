d1663efb975807e2aa34190d3aacdd3d23bfeb21
Nicky Meuleman <nicky.dev@outlook.com>
2025-03-16 20:49:11 +0100

    nvim: add keymaps

--- nvim/.config/nvim/lua/nicky/keymaps.lua
+++ nvim/.config/nvim/lua/nicky/keymaps.lua
@@ -1,7 +1,11 @@
--- window navigation
-vim.keymap.set("n", "<C-h>", "<C-w>h", {})
-vim.keymap.set("n", "<C-j>", "<C-w>j", {})
-vim.keymap.set("n", "<C-k>", "<C-w>k", {})
-vim.keymap.set("n", "<C-l>", "<C-w>l", {})
+vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Focus window to the left" })
+vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Focus window to the bottom" })
+vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Focus window to the top" })
+vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Focus window to the right" })
 
-vim.keymap.set("v", "<leader>y", [["+y]], { desc = "Yank into sytem clipboard" })
+vim.keymap.set({ "n", "v" }, "<leader>p", [["+p]], { desc = "Paste from sytem clipboard" })
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]], { desc = "Yank into sytem clipboard" })
+vim.keymap.set("n", "<leader>Y", [["+Y]], { desc = "Yank line into system clipboard" })
+
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move selected line up" })
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move selected line down" })
